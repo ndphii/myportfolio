c64cba670c1739c081ba34826a97b703ffeb4019
ndphii <phind76.main@gmail.com>
2024-02-22 01:31:14 +0700

    22/2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
 import logo from './logo.svg';
+import iconMouse from "./asset/icon/mouse.svg"
+import iconEyeVisible from './asset/icon/eyevisible.svg'
 import './App.css';
+import React, { useEffect, useState, useRef } from 'react';
+// import Test from './componets/test';
+function useMouse() {
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0
+  });
+
+  useEffect(() => {
+    function handle(e) {
+      setMousePosition({
+        x: e.pageX,
+        y: e.pageY
+      });
+    }
+
+    document.addEventListener("mousemove", handle);
+
+    return () => document.removeEventListener("mousemove", handle);
+  }, []);
+
+  return mousePosition;
+}
+
+window.addEventListener("load", (e) => {
+  // thêm đoạn text chạy tròn quanh con chuột
+  const str = " ゜Graphic Design  ゜ UI/UX Design ";
+  const text = document.querySelector(".text-block-outside");
+  for (let i = 0; i < str.length; i++) {
+    let span = document.createElement("span");
+    span.innerHTML = str[i];
+    text.appendChild(span);
+    span.style.transform = `rotate(${11 * i}deg) `;
+  }
+
+});
 
 function App() {
+  const { x, y } = useMouse();
+  const [svgMouse, setSvgMouse] = useState({
+    src: iconMouse,
+  })
+  const [isHovered, setIsHovered] = useState(false);
+  const [cursorInsideStyle, setCursorInsideStyle] = useState({
+    transform: 'translate(-50%, -50%) scale(1.0)',
+
+  });
+
+  const aRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    setCursorInsideStyle({
+      transform: 'translate(-50%, -50%) scale(1.4)',
+    });
+    setSvgMouse({
+      src: iconEyeVisible,
+    })
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    setCursorInsideStyle({
+      transform: 'translate(-50%, -50%) scale(1.0)',
+    });
+    setSvgMouse({
+      src: iconMouse,
+    })
+  };
+
+  useEffect(() => {
+    const cursorOutside = document.querySelector(".cursor-outside");
+    const cursorInside = document.querySelector(".cursor-inside");
+    if (cursorOutside && cursorInside) {
+      cursorOutside.style.cssText = cursorInside.style.cssText = `left: ${x}px; top: ${y}px;`;
+      cursorInside.style.transform = cursorInsideStyle.transform;
+    }
+  }, [x, y, cursorInsideStyle]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      {/* <Test /> */}
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="" />
+        <div onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}>
+          {isHovered.toString()}
+        </div>
         <a
+          ref={aRef}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >link
         </a>
-      </header>
+        <div className="cursor-outside">
+          <p className="text-block-outside"></p>
+        </div>
+        <div className="cursor-inside">
+          <p className="svg-inside">
+            <img className="iconMouse" src={svgMouse.src} alt="" />
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/componets/test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import "../style/test.css"
+class Test extends React.Component {
+    render() {
+        return (
+            <p className="" id="text" >
+                "Đây là frame Contact"
+            </p >
+        );
+    }
+}
+
+export default Test;
